58714db24c4dbd8043ad8dedc7281666c0460497
Vladimir Dulenov <originalblack@mail.ru>
2018-09-25 15:51:59 +0300

    add getter ads_block

--- main_parser.py
+++ main_parser.py
@@ -13,10 +13,15 @@ def get_items_titles(html):
         ads_lst.append(el.get_text())
     return ads_lst
 
+def get_ad_blocks(html):
+    soup = BeautifulSoup(html, 'html.parser')
+    print(soup.find_all("div", {"class": "item item_table clearfix js-catalog-item-enum js-item-extended item_table_extended snippet-experiment item_hide-elements"}))
+
 
 def main():
     url = 'https://www.avito.ru/moskva/koshki/abissinskaya?s_trg=1007'
-    print(get_items_titles(get_html(url)))
+    #print(get_items_titles(get_html(url)))
+    get_ad_blocks(get_html(url))
 
 
 if __name__ == '__main__':
