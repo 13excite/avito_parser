17e68077f0b2be46da9067604667523cc6c5d495
Vladimir Dulenov <originalblack@mail.ru>
2018-09-26 01:26:29 +0300

    done working parser first page hardcode cat breed

--- main_parser.py
+++ main_parser.py
@@ -1,17 +1,28 @@
+import csv
 from requester import get_html
 from bs4 import BeautifulSoup
 import re
 
 
-def get_ad_info(html):
-    soup = BeautifulSoup(html, 'html.parser')
+def get_ad_info(html, write_to_file='cats_avito.csv'):
+    """
+    parsing pages in xml format page by page and writing data to csv file
+    """
+    # parser working only xml format, if init BeautifulSoup with second args html.parser, then
+    # we get only first element from item_table(html has a error in markup)
+    soup = BeautifulSoup(html, 'lxml')
     ads = soup.find('div', class_='catalog-list').find_all('div', class_='item_table')
-    print(ads)
     for ad in ads:
         title_and_link = ad.find('div', class_='description').find('h3')
         title = title_and_link.text.strip()
-        add_link = 'https://avito.ru' + title_and_link.find('a').get('href')
+        ad_link = 'https://avito.ru' + title_and_link.find('a').get('href')
         try:
+            # match id from ad link url
+            ad_id = re.match('.*\_(\d+$)', ad_link).group(1)
+        except Exception:
+            ad_id = ''
+        try:
+            # get ad thumbs url, need for upload in storage
             thumb_link = "https:%s" % re.findall(r"\/\/\d+.*jpg", str(ad.find('div', class_='item-slider-image')))[0]
         except Exception:
             thumb_link = ''
@@ -23,17 +34,16 @@ def get_ad_info(html):
             address = ad.find('div', class_='data').find_all('p')[-1].text.strip()
         except Exception:
             address = ''
-        print(title)
-        print(add_link)
-        print(price)
-        print(address)
-        print(thumb_link)
+        ad_data = {
+            'id':       ad_id,
+            'title':    title,
+            'price':    price,
+            'address':  address,
+            'ad_url':   ad_link,
+            'ad_url':  thumb_link,
+        }
+        write_to_csv(write_to_file, ad_data)
 
-
-#def degug_get(html):
-#   soup = BeautifulSoup(html, 'html.parser')
-#    ads = soup.find('div', class_='catalog-list')
-#    print(ads)
 
 def get_pages_count(html):
     soup = BeautifulSoup(html, 'html.parser')
@@ -41,14 +51,26 @@ def get_pages_count(html):
     last_page_num = pages.split('=')[1].split('&')[0]
     return int(last_page_num)
 
+
+def write_to_csv(filename, data, mode='a'):
+    try:
+        with open(filename, mode) as f:
+            w = csv.writer(f, delimiter=';')
+            w.writerow((data['id'], data['title'], data['price'],
+                       data['address'], data['ad_url'], data['ad_url']))
+    except csv.Error as err:
+        print('file {}: {}'.format(filename, err))
+    except Exception as err:
+        print("Unexpected error writing cvf file: ", err)
+
+
 def main():
+    # get total pages count from first page
     url = 'https://www.avito.ru/moskva/koshki/abissinskaya?p=1'
     page_html = get_html(url)
-    print(page_html)
-    #print(get_items_titles())
-    #get_ad_info(page_html)
+    #last_page = get_pages_count(page_html)
+    get_ad_info(page_html)
     #get_pages_count(page_html)
-    #degug_get(page_html)
 
 
 if __name__ == '__main__':
