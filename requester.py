6836de5dd1c0f8721d9f92e61eee0b19a87cba91
Vladimir Dulenov <originalblack@mail.ru>
2018-09-25 21:37:48 +0300

    fix referer header

--- requester.py
+++ requester.py
@@ -7,7 +7,7 @@ def get_html(url, timeout=5):
         'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
         'accept-encoding': 'gzip, deflate, br',
         'accept-language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q = 0.7',
-        'referer': 'https: // www.avito.ru / moskva',
+        'referer': 'https://www.avito.ru/moskva',
         'User-Agent': 'Mozilla/5.0 (Windows; U; Windows NT 5.1; it; rv:1.8.1.11) Gecko/20071127 Firefox/2.0.0.11',
     }
     try:
