0a08c89acda93619d182960c0f297dd5add598dd
Vladimir Dulenov <originalblack@mail.ru>
2018-09-27 01:43:52 +0300

    many refactoring, add working pipeline for add_list->file.csv

--- main_parser.py
+++ main_parser.py
@@ -1,48 +1,56 @@
 import csv
+import re
+import time
+from ad_parser import get_ad_info
+from breed_parser import get_breed
 from requester import get_html
 from bs4 import BeautifulSoup
-import re
 
 
-def get_ad_info(html, write_to_file='cats_avito.csv'):
+def get_base_info(html, breed, write_to_file='cats_avito.csv'):
     """
     parsing pages in xml format page by page and writing data to csv file
     """
     # parser working only xml format, if init BeautifulSoup with second args html.parser, then
     # we get only first element from item_table(html has a error in markup)
     soup = BeautifulSoup(html, 'lxml')
-    ads = soup.find('div', class_='catalog-list').find_all('div', class_='item_table')
-    for ad in ads:
-        title_and_link = ad.find('div', class_='description').find('h3')
-        title = title_and_link.text.strip()
-        ad_link = 'https://avito.ru' + title_and_link.find('a').get('href')
-        try:
-            # match id from ad link url
-            ad_id = re.match('.*\_(\d+$)', ad_link).group(1)
-        except Exception:
-            ad_id = ''
-        try:
-            # get ad thumbs url, need for upload in storage
-            thumb_link = "https:%s" % re.findall(r"\/\/\d+.*jpg", str(ad.find('div', class_='item-slider-image')))[0]
-        except Exception:
-            thumb_link = ''
-        try:
-            price = ad.find('div', class_='about').text.strip()
-        except Exception:
-            price = ''
-        try:
-            address = ad.find('div', class_='data').find_all('p')[-1].text.strip()
-        except Exception:
-            address = ''
-        ad_data = {
-            'id':       ad_id,
-            'title':    title,
-            'price':    price,
-            'address':  address,
-            'ad_url':   ad_link,
-            'thb_url':  thumb_link,
-        }
-        write_to_csv(write_to_file, ad_data)
+    try:
+        ads = soup.find('div', class_='catalog-list').find_all('div', class_='item_table')
+        for ad in ads:
+            title_and_link = ad.find('div', class_='description').find('h3')
+            #title = title_and_link.text.strip()
+            ad_link = 'https://avito.ru' + title_and_link.find('a').get('href')
+            try:
+                # match id from ad link url
+                ad_id = re.match('.*\_(\d+$)', ad_link).group(1)
+            except Exception:
+                ad_id = ''
+            try:
+                # get ad thumbs url, need for upload in storage
+                thumb_link = "https:%s" % re.findall(r"\/\/\d+.*jpg", str(ad.find('div', class_='item-slider-image')))[0]
+            except Exception:
+                thumb_link = ''
+            #try:
+            #    price = ad.find('div', class_='about').text.strip()
+            #except Exception:
+            #    price = ''
+            #try:
+            #    address = ad.find('div', class_='data').find_all('p')[-1].text.strip()
+            #except Exception:
+            #    address = ''
+            ad_data = {
+                'id':       ad_id,
+                #'title':    title,
+                #'price':    price,
+                #'address':  address,
+                'ad_url':   ad_link,
+                'thb_url':  thumb_link,
+                'breed':    breed,
+            }
+            write_to_csv(write_to_file, ad_data)
+    except Exception as err:
+        print("Zabanili")
+        print('Error: ', err)
 
 
 def get_pages_count(html):
@@ -59,8 +67,7 @@ def write_to_csv(filename, data, mode='a'):
     try:
         with open(filename, mode) as f:
             w = csv.writer(f, delimiter=';')
-            w.writerow((data['id'], data['title'], data['price'],
-                       data['address'], data['ad_url'], data['thb_url']))
+            w.writerow((data['id'],  data['ad_url'], data['thb_url'], data['breed']))
     except csv.Error as err:
         print('file {}: {}'.format(filename, err))
     except Exception as err:
@@ -72,15 +79,29 @@ def get_cats_breed(html):
 
 
 def main():
-    # get total pages count from first page
-    url = 'https://www.avito.ru/moskva/koshki/abissinskaya?p=1'
-    last_page = get_pages_count(get_html(url))
-    # need add dynamic cat breed
-    default_url = 'https://www.avito.ru/moskva/koshki/abissinskaya?p=%s'
+    # get breed urls
+    for breed, breed_url in get_breed().items():
+        first_breed_page = breed_url % '1'
+        last_page = get_pages_count(get_html(first_breed_page))
+        for page_num in range(1, last_page + 1):
+            generic_url = breed_url % page_num
+            get_base_info(get_html(generic_url), breed)
+        time.sleep(5)
 
-    for page_num in range(1, last_page + 1):
-        generic_url = default_url % page_num
-        get_ad_info(get_html(generic_url))
+
+
+
+
+    # get total pages count from first page
+    #url = 'https://www.avito.ru/moskva/koshki/abissinskaya?p=1'
+    #last_page = get_pages_count(get_html(url))
+
+    # need add dynamic cat breed
+    #default_url = 'https://www.avito.ru/moskva/koshki/abissinskaya?p=%s'
+
+    #for page_num in range(1, last_page + 1):
+    #    generic_url = default_url % page_num
+    #    get_base_info(get_html(generic_url))
 
 
 if __name__ == '__main__':
