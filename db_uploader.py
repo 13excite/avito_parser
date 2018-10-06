19f9ccc997f3f87c3bfefa63a6736d92ae89d84d
Vladimir Dulenov <originalblack@mail.ru>
2018-10-06 16:21:37 +0300

    fix many bugs, deleted debug info

--- db_uploader.py
+++ db_uploader.py
@@ -1,8 +1,10 @@
+import argparse
 import csv
 from datetime import datetime, timedelta
 from db.db_executor import DbExecutor, get_auth
 import random
 import re
+import uuid
 
 
 def random_time_generator():
@@ -26,10 +28,9 @@ def convert_to_date_format(date_string):
         date = "%s %s" % (datetime.now().strftime("%Y-%m-%d"), date_string.split()[2])
     elif yesterday_pattern.match(date_string):
         date = "%s %s" % (datetime.strftime(datetime.now() - timedelta(1), '%Y-%m-%d'), date_string.split()[2])
-        print(date)
     else:
-        date = datetime.now().strftime("%Y-%m-%d %H:%M")
-    return  date
+        date = (datetime.now() - timedelta(minutes=15)).strftime("%Y-%m-%d %H:%M")
+    return date
 
 
 def csv_reader(file):
@@ -37,21 +38,24 @@ def csv_reader(file):
         with open(file, 'r') as f:
             reader = csv.reader(f, delimiter=";")
             for row in reader:
-                id = row[0]
+                if row[0]:
+                    id = row[0]
+                else:
+                    id = uuid.uuid1().fields[0]
                 title = row[1]
                 date = str(convert_to_date_format(row[2]))
                 img_url = row[3]
                 if row[4]:
                     price = row[4]
                 else:
-                    price='NULL'
+                    price = 'NULL'
                 address = row[5]
-                desc = row[6]
+                desc = row[6].replace("'", "")
                 breed = row[7]
                 query = "INSERT INTO items (id, title, start_date, img_url, price, address, description, breed)" \
-                        "VALUES (%s, \'%s\', \'%s\', \'%s\', \'%s\', \'%s\', \'%s\', \'%s\' )" % (id, title, date, img_url, price, address, desc, breed)
+                        "VALUES (%s, \'%s\', \'%s\', \'%s\', \'%s\', \'%s\', \'%s\', \'%s\' );" % (id, title, date, img_url, price, address, desc, breed)
 
-                print(query)
+                yield query
     except IOError as err:
         print('I/O Error: ', err)
     except Exception as err:
@@ -59,7 +63,15 @@ def csv_reader(file):
 
 
 def main():
-    csv_reader('total_full_ad.csv')
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-c', '--config', type=str, help='usage -c /path/dbconf.ini ', required=True)
+    args = parser.parse_args()
+
+    auth_dict = get_auth(args.config)
+    db = DbExecutor(auth_dict)
+
+    for q in csv_reader('total_full_ad.csv'):
+        db.query_insert(q)
 
 
 if __name__ == '__main__':
